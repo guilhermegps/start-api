578af4bf8da0167e67871a2f3f5b770dd6d299ea
Guilherme Gps <guilherme.ps20@gmail.com>
2025-02-19 09:26:11 -0300

    FEAT - Converting Liquibase xml to plain sql

---
+++ src/main/resources/db/changelog/1.000.00__CREATE_TABLES.sql
@@ -0,0 +1,45 @@
+--liquibase formatted sql
+--changeset guilherme.gps:1.000.00__CREATE_TABLES
+CREATE TABLE START.perfil (
+	id uuid DEFAULT RANDOM_UUID() NOT NULL,
+	codigo INT GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	ativo bool DEFAULT true NOT NULL,
+	descricao varchar(50) NOT NULL,
+	CONSTRAINT pk_perfil PRIMARY KEY (id)
+);
+
+CREATE TABLE START.usuario (
+	id uuid DEFAULT RANDOM_UUID() NOT NULL,
+	codigo INT GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	ativo bool DEFAULT true NOT NULL,
+	id_perfil uuid NULL,
+	login varchar(50) NOT NULL UNIQUE,
+	nome varchar(100) NOT NULL,
+	cpf varchar(11) NOT NULL UNIQUE,
+	senha varchar(60) NOT NULL,
+	email varchar(50) NOT NULL UNIQUE,
+	CONSTRAINT pk_usuario PRIMARY KEY (id),
+	CONSTRAINT fk_usuario_perfil FOREIGN KEY (id_perfil) REFERENCES START.perfil(id)
+);
+
+CREATE TABLE START.tipo_evento (
+	id uuid DEFAULT RANDOM_UUID() NOT NULL,
+	codigo INT GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	ativo bool DEFAULT true NOT NULL,
+	descricao varchar(60) NOT NULL,
+	CONSTRAINT pk_tipo_evento PRIMARY KEY (id)
+);
+
+CREATE TABLE START.evento (
+	id uuid DEFAULT RANDOM_UUID() NOT NULL,
+	codigo INT GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	ativo bool DEFAULT true NOT NULL,
+	data_registro timestamp DEFAULT now() NOT NULL,
+	id_usuario uuid NOT NULL,
+	id_tipo_evento uuid NOT NULL,
+	descricao varchar(255) NULL,
+	ip_usuario varchar(40) NULL,
+	CONSTRAINT pk_evento PRIMARY KEY (id),
+	CONSTRAINT fk_evento_tipo_evento FOREIGN KEY (id_tipo_evento) REFERENCES START.tipo_evento(id),
+	CONSTRAINT fk_evento_usuario FOREIGN KEY (id_usuario) REFERENCES START.usuario(id)
+);
