e9cdee8efb8d22e8c6ff75f9d68a44d1aab1bfeb
Guilherme Gps <guilherme.ps20@gmail.com>
2025-02-19 16:07:49 -0300

    FEAT - Authentication example using basic auth

---
+++ src/main/java/com/project/start/api/commons/configs/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.project.start.api.commons.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.project.start.api.commons.filters.CustomBasicAuthFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+    protected SecurityFilterChain filterChain(
+            HttpSecurity http,
+            CustomBasicAuthFilter customBasicAuthFilter) throws Exception {
+        http.authorizeHttpRequests(
+                authorizeConfig -> {
+                    authorizeConfig.requestMatchers("/public").permitAll();
+                    authorizeConfig.anyRequest().authenticated();
+                }
+        )
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(customBasicAuthFilter, BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+}
